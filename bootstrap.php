21395bb09cacbce9925b665b5968104d2632625b
Glooerli <luki.dc@hotmail.com>
2016-02-15 13:12:07 +0100

    Line changed

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,16 @@ namespace roobique
     use roobique\Routers\Router;
     use roobique\ValueObjects\Uri;
 
+    require __DIR__ . '/src/autoload.php';
+
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
     date_default_timezone_set('Europe/Zurich');
 
-    require __DIR__ . '/src/autoload.php';
-    
+
+
     $router = new Router;
+
     $uri = new Uri('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 
     $router->route($uri)->run($uri)->send();
