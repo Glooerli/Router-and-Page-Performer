e9cab7189baebbcbc26046743917d510e21a0b77
Glooerli <luki.dc@hotmail.com>
2016-03-04 06:05:41 +0100

    ddd

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 namespace oop_Learn
 {
     use oop_Learn\Routers\Router;
     use oop_Learn\ValueObjects\Uri;
-=======
-
-namespace oop_Learn
-{
-
-    use oop_Learn\Routers\Router;
->>>>>>> origin/master
 
     require __DIR__ . '/src/autoload.php';
 
@@ -19,17 +11,8 @@ namespace oop_Learn
     error_reporting(E_ALL);
     date_default_timezone_set('Europe/Zurich');
 
-<<<<<<< HEAD
     $router = new Router;
     $uri = new Uri('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 
     $router->route($uri)->run($uri)->send();
 }
-=======
-    $router = new Router();
-    $uri = $_SERVER['REQUEST_URI'];
-
-    $router->route($uri);
-
-}
->>>>>>> origin/master
