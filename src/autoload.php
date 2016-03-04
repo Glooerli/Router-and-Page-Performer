d82789e253dcbd9ca3329aa6163f90e496407578
Glooerli <luki.dc@hotmail.com>
2016-03-04 06:05:21 +0100

    dd

--- src/autoload.php
+++ src/autoload.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 spl_autoload_register(
     function($class) {
         static $classes = null;
@@ -11,12 +8,8 @@ spl_autoload_register(
             $classes = array(
                 'oop_learn\\controllers\\frontpagecontroller' => '/Controllers/FrontPageController.php',
                 'oop_learn\\controllers\\abstractcontroller' => '/Controllers/AbstractController.php',
-<<<<<<< HEAD
                 'oop_learn\\loaders\\loader' => '/Loaders/Loader.php',
                 'oop_learn\\responses\\response' => '/Responses/Response.php',
-=======
-                'oop_learn\\loaders\\pagedomloader' => '/Loaders/PageDOMLoader.php',
->>>>>>> origin/master
                 'oop_learn\\routers\\router' => '/Routers/Router.php',
                 'oop_learn\\valueobjects\\uri' => '/ValueObjects/Uri.php',
             );
